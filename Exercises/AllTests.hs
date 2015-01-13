a4cf306bfbd0a55d108242575eb19bb34004852c
Erlend Hamberg <erlend@hamberg.no>
2015-01-13 19:29:29 +0100

    Rearrange tests to be in the same order as in the README

--- Exercises/AllTests.hs
+++ Exercises/AllTests.hs
@@ -7,29 +7,27 @@ module Main where
 import Test.QuickCheck
 import Test.Hspec
 
+import qualified Tests.GettingStartedTests            as GS
 import qualified Tests.CreditCardNumberValidatorTests as CV
 import qualified Tests.FunctionsTests                 as FU
-import qualified Tests.GettingStartedTests            as GS
-import qualified Tests.HigherOrderFunctionsTests      as HF
 import qualified Tests.PatternMatchingTests           as PM
+import qualified Tests.HigherOrderFunctionsTests      as HF
 import qualified Tests.ProjectEulerTests              as PE
-import qualified Tests.QuickCheckExamplesTests        as QC
-import qualified Tests.RecursionTests                 as RC
-import qualified Tests.RecursionSchemesTests          as RS
 import qualified Tests.RecursionTests                 as RE
+import qualified Tests.RecursionSchemesTests          as RS
+import qualified Tests.QuickCheckExamplesTests        as QC
 import qualified Tests.VigenereTests                  as VI
 
 
 main :: IO ()
 main = hspec $ do
+    GS.tests
     CV.tests
     FU.tests
-    GS.tests
-    HF.tests
     PM.tests
+    HF.tests
     PE.tests
-    QC.tests
-    RC.tests
-    RS.tests
     RE.tests
+    RS.tests
+    QC.tests
     VI.tests
