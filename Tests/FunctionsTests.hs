832abe3b8424fa0117c4fb2e8e466c0e3923edfa
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2015-01-14 13:33:06 +0100

    Rearrange order of tests to follow code examples

--- Tests/FunctionsTests.hs
+++ Tests/FunctionsTests.hs
@@ -17,17 +17,17 @@ tests = do
     describe "Functions.sum3" $ do 
         it "adds three *arbitrary* numbers" $ do
             QC.property $ \x y z -> FU.sum3 x y z == x + y + z
-
+    
+    describe "Functions.isDollar" $ do 
+        it "True only for '$'" $ do
+            QC.property $ \c -> if c == '$' then FU.isDollar c else FU.isDollar c == False
+    
     describe "Functions.xor" $ do 
         it "xor works as expected" $ do
             [FU.xor False False,
              FU.xor False True,
              FU.xor True  False,
              FU.xor True  True] == [False, True, True, False]
-
-    describe "Functions.isDollar" $ do 
-        it "True only for '$'" $ do
-            QC.property $ \c -> if c == '$' then FU.isDollar c else FU.isDollar c == False
 
 main = hspec tests
 
